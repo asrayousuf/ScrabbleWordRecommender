ca756a7d8081fd973a54e305e84c0f6b84bd7628
asrayousuf <asrayousuf28@gmail.com>
2015-07-24 15:39:03 +0530

    Added provision to accept user input

--- ScrabbleWordSuggester.java
+++ ScrabbleWordSuggester.java
@@ -1,30 +1,43 @@
 package ScrabbleTeam3;
-	import java.io.BufferedWriter;
-	import java.io.File;
-	import java.io.FileNotFoundException;
-	import java.io.FileWriter;
-	import java.io.IOException;
-	import java.io.Writer;
-	import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 
 	public class ScrabbleWordSuggester
 {
 	static final int SCORE_OF_LETTERS[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
-	static final int NUMBER_OF_SUGGESTIONS_NEEDED=5;
 	static final String FILE_PATH = "C:\\Users\\test\\workspace\\BootCamp\\src\\ScrabbleTeam3\\sowpods.txt";
-	ArrayList<Word> dictionary;
+	static ArrayList<Word> dictionary;
 	String rack;
-
-	ScrabbleWordSuggestor(String rack)
+	ScrabbleWordSuggester(String rack)
 	{
 		buildDictionary();
 		sortDictionaryDescendingly();
 		this.rack = rack;
 	}
 
-	public static void main(String args[])
+	public static void main(String args[]) throws IOException
 	{
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Enter String");
+		ScrabbleWordSuggester scrabbleWordSuggester = new ScrabbleWordSuggester(scanner.nextLine());
+		System.out.println("Do you wish to consider any constraints: Y/N");
+		String answer= scanner.nextLine();
+		String constraint;
+				if(answer.equalsIgnoreCase("Y")){
+					System.out.println("Enter your constraint");
+					constraint=scanner.nextLine();
+				}
+		
+		System.out.println("Enter number of suggested words to be returned");
+		int NUMBER_OF_SUGGESTIONS_NEEDED=scanner.nextInt();
+		
 		printSuggestions(findPossibleWords(dictionary, rack));
 	}
 
@@ -57,27 +70,4 @@ package ScrabbleTeam3;
 		Collections.sort(dictionary);
 	}
 
-}
-
-class Word implements Comparable<Word>
-{
-	String value;
-	int key;
-	int length;
-
-	public Word(String value, int key)
-	{
-		this.value = value;
-		this.key = key;
-		this.length = value.length();
-	}
-	@Override
-	public int compareTo(Word o)
-	{
-		if(this.key == o.key)
-		{
-			return (this.value.compareTo(o.value));
-		}
-		return o.key-this.key;
-	}
 }
