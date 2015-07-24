6db2404b9ac7c35537633eb5f78fdeda7730d308
Aravind <aravind.me001@gmail.com>
2015-07-24 14:45:13 +0530

    Added word class and variable names changed

--- ScrabbleWordSuggester.java
+++ ScrabbleWordSuggester.java
@@ -9,38 +9,75 @@ package ScrabbleTeam3;
 
 
 	public class ScrabbleWordSuggester
-	{
-		static final int SCORE_OF_LETTERS[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
-		static final int NUMBER_OF_SUGGESTIONS_NEEDED=5;
-		static final String FILE_PATH = "C:\\Users\\test\\workspace\\BootCamp\\src\\ScrabbleTeam3\\sowpods.txt";
-		
-		public static void main(String args[])
-		{
-			List<Word> listOfWords = readFile(FILE_PATH);
-			listOfWords = wordSortbyScore(listOfWords);
-			String rack = " c i";
-			printSuggestions(findPossibleWords(listOfWords, rack));
-		}
-		
-		static List<Word> readFile(String path)
-		{
-			List<Word> listOfWords = new ArrayList<Word>();
-			try
-			{
-				Scanner sc = new Scanner(new File(path));
-				while(sc.hasNext())
-				{
-					String word = sc.next();
-					Word wordWithScore = new Word(word, calculateScore(word));
-					listOfWords.add(wordWithScore);
-				}	
-			} 
-			catch (FileNotFoundException e)
-			{
-				e.printStackTrace();
-			}
-			return listOfWords;
-		}
+{
+	static final int SCORE_OF_LETTERS[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
+	static final int NUMBER_OF_SUGGESTIONS_NEEDED=5;
+	static final String FILE_PATH = "C:\\Users\\test\\workspace\\BootCamp\\src\\ScrabbleTeam3\\sowpods.txt";
+	ArrayList<Word> dictionary;
+	String rack;
 
+	ScrabbleWordSuggestor(String rack)
+	{
+		buildDictionary();
+		sortDictionaryDescendingly();
+		this.rack = rack;
 	}
 
+	public static void main(String args[])
+	{
+		printSuggestions(findPossibleWords(dictionary, rack));
+	}
+
+	private void buildDictionary()
+	{
+		dictionary = new ArrayList<Word>();
+		try {
+			Scanner sc = new Scanner(new File(FILE_PATH));
+			while ( sc.hasNext() ) {
+				String word = sc.next();
+				Word wordWithScore = new Word(word, calculateScore(word));
+				dictionary.add(wordWithScore);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int calculateScore(String word) {
+		int value = 0;
+		for (int i = 0; i < word.length(); i++ )
+		{
+			char c = word.charAt(i);
+			value += SCORE_OF_LETTERS[c-'A'];
+		}
+		return value;
+	}
+
+	void sortDictionaryDescendingly() {
+		Collections.sort(dictionary);
+	}
+
+}
+
+class Word implements Comparable<Word>
+{
+	String value;
+	int key;
+	int length;
+
+	public Word(String value, int key)
+	{
+		this.value = value;
+		this.key = key;
+		this.length = value.length();
+	}
+	@Override
+	public int compareTo(Word o)
+	{
+		if(this.key == o.key)
+		{
+			return (this.value.compareTo(o.value));
+		}
+		return o.key-this.key;
+	}
+}
