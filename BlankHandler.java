be6035287732de89a98bddd2032c1d2fa6dcce36
Sri Harish <test@L-MAA-25004247.paypalcorp.com>
2015-07-24 19:03:51 +0530

    Changed BlankHandling to BlankHandler

--- BlankHandler.java
+++ BlankHandler.java
@@ -13,7 +13,7 @@ class BlankHandler {
 	static final int SCORE_OF_LETTERS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 	static final int A_ASCII = 97;
 	
-	public BlankHandling() {
+	public BlankHandler() {
 		rackWithoutBlanks = "";
 	}
 	
