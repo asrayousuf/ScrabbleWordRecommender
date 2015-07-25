6981dc164c18e4e3f6120155d75b167a87123684
asrayousuf <asrayousuf28@gmail.com>
2015-07-25 12:04:00 +0530

    changed constant 'a' to 'A'

--- ScrabbleWordListBuilder.java
+++ ScrabbleWordListBuilder.java
@@ -10,7 +10,7 @@ import java.util.TreeMap;
 
 public class ScrabbleWordListBuilder {
 	static final int SCORE_OF_LETTERS[] = {1,3,3,2,1,4,2,4,1,8,5,1,3,1,1,3,10,1,1,1,1,4,4,8,4,10};
-	static final char CHARACTER_A = 'a';
+	static final char CHARACTER_A = 'A';
 	static final String FILE_PATH = "C:\\Users\\test\\workspace\\BootCamp\\src\\ScrabbleTeam3\\sowpods.txt";
 	private HashMap<String, String> wordList = new HashMap<String, String>();
 	
