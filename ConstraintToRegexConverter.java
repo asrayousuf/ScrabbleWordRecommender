745650a12c7b29afa24c4c8964b6d3b8009d589f
jayasruthi <jayasruthivr@gmail.com>
2015-07-25 11:51:56 +0530

    Fixing bug in regex

--- ConstraintToRegexConverter.java
+++ ConstraintToRegexConverter.java
@@ -8,50 +8,21 @@ public class ConstraintToRegexConverter
 {	
 	 public static String convertToRegex(String constraint)
 	 {
-	      StringBuilder regex = new StringBuilder("");    
-	      int i=0;
-	      while (constraint.charAt(i) == '*' && i < constraint.length())
-	      {
-	    		i++;
-	      }
-	      int firstChar = i;
-	      int lastChar = 0;
-	      while (i != constraint.length())
-	      {
-	    	    if(constraint.charAt(i)!='*')
-	    		{
-	    	    	lastChar = i;
-	    		}
-	    		i++;
-	      }
-	      if (firstChar != 0)
-	      {
-	    	  regex.append("(.*)");
-	      }
-	      for (int j = firstChar; j <= lastChar; j++)
-	      {
-	    		if(constraint.charAt(j) != '*')
-	    		{	
-	    		    regex.append(constraint.charAt(j));
-	    		}
-	    		else
-	    		{	
-	    			regex.append("(.)");
-	    		}
-	      }
-	      if (lastChar != constraint.length() - 1)
-	      {
-	    		regex.append("(.*)");
-	      }  
-	      return regex.toString();
+	      StringBuilder regex = new StringBuilder("");
+	     for (int i = 0; i < constraint.length() ; i++)
+	     {
+	    	 if (constraint.charAt(i) == '.') regex.append('.');
+	    	 else if (constraint.charAt(i) == '*') regex.append("([a-z])*");
+	    	 else regex.append(constraint.charAt(i));
+	     }
+	       return regex.toString();
 	   }
 	 
 	 static boolean isMatching(String word, String regex)
 	 {
 		  Pattern pattern = Pattern.compile(regex);
 	      Matcher matcher = pattern.matcher(word);
-	      
-	      return (matcher.find( ));
+	      return matcher.matches();
 	 }
 	 
 }
